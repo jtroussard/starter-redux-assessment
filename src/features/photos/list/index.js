5a331b68578a4b26b7d8956d8722bbf9d3bcd8b7
Jacques Troussard <mm_sparrow@beeswaxnoneofyours.com>
2024-09-01 15:38:28 -0400

    finished assignment

--- src/features/photos/list/index.js
+++ src/features/photos/list/index.js
@@ -1,28 +1,31 @@
 import { useSelector, useDispatch } from 'react-redux';
 import {
   // Task 7: Import the `removePhoto()` action creator from the photos slice
-  selectAllPhotos,
+  removePhoto,
   // Task 13: Import the `selectFilteredPhotos()` selector from the photos slice
+  selectFilteredPhotos
 } from '../photos.slice';
 import './list.css';
 
 export default function PhotosList() {
   // Task 14: Call `useSelector()` below with `selectFilteredPhotos` instead of `selectAllPhotos`
-  const photos = useSelector(selectAllPhotos);
+  const photos = useSelector(selectFilteredPhotos);
   // Task 8: Store a reference to the Redux store's dispatch method in a variable called `dispatch`
+  const dispatch = useDispatch();
 
-  function handleDeleteButtonClick(id) {
+  function handleDeleteButtonClick(id, caption) {
     // Task 9: Dispatch the `removePhoto()` action creator, passing in the id
+    dispatch(removePhoto({id: id, caption: caption})) // I think some redux magic includes the state everytime an action is dispatched.
   }
 
-  const photosListItems = photos.map(({ id, caption, imageUrl }) => (
-    <li key={id}>
+  const photosListItems = photos.map(({ id, caption, imageUrl }, index) => (
+    <li key={`${id}-${index}`}>
       <img alt={caption} src={imageUrl} />
       <div>
         <p>{caption}</p>
         <button
           data-testid={`${caption}-button`}
-          onClick={() => handleDeleteButtonClick(id)}>
+          onClick={() => handleDeleteButtonClick(id, caption)}>
           Delete
         </button>
       </div>
--- src/features/photos/photos.slice.js
+++ src/features/photos/photos.slice.js
@@ -13,10 +13,20 @@ const options = {
     // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos. 
     // Task 1 Hint: You can use state.photos.unshift()
     // `unshift()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
+    addPhoto(state, action) {
+      state.photos.unshift(action.payload) // unshift returns a new list, and immur is in play as well
+    },
    
     // Task 6: Create an `removePhoto()` case reducer that removes a photo from state.photos
     // Task 6 Hint: You can use state.photos.splice()
     // `splice()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+    removePhoto(state, action) {
+      const { id, caption } = action.payload;
+      const index = state.photos.findIndex(photo => photo.id === id && photo.caption === caption);
+      if (index !== -1) {
+        state.photos.splice(index, 1);
+      }
+    }
   },
 };
 
@@ -29,4 +39,7 @@ export default photosSlice.reducer;
 export const selectAllPhotos = (state) => state.photos.photos;
 export const selectFilteredPhotos = (state) => {
   // Task 12: Complete `selectFilteredPhotos()` selector to return a filtered list of photos whose captions match the user's search term
+  const filtered = selectAllPhotos(state).filter((photo => { return photo.caption.toLowerCase().includes(selectSearchTerm(state).toLowerCase()) }))
+  // console.log(`BUT SNATCHER | ${filtered} | SELECTED PHOTOS = ${JSON.stringify(selectAllPhotos(state)filter())} | ${JSON.stringify(state.photos)}`);
+  return filtered;
 };
